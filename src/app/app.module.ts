0a718fb46b111a8568ff74b3431da32fd402af78
Mauro <mau.pinop@alumnos.duoc.cl>
2018-01-21 13:44:32 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
+import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ImagenService } from './servicios/imagen.service'
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { ListaImagenComponent } from './galeria/lista-imagen/lista-imagen.component';
+import { ImagenComponent } from './galeria/lista-imagen/imagen.component';
+import { PedidoDetailComponent } from './galeria/pedido-detail/pedido-detail.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { SolicitudesComponent } from './solicitudes/solicitudes.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdminImagenListComponent } from './admin/admin-imagen-list/admin-imagen-list.component';
+import { DashboardComponent } from './admin/dashboard.component';
+import { AdminImagenCreateComponent } from './admin-imagen-create/admin-imagen-create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GaleriaComponent,
+    ListaImagenComponent,
+    ImagenComponent,
+    PedidoDetailComponent,
+    QuienesSomosComponent,
+    SolicitudesComponent,
+    AdminComponent,
+    AdminImagenListComponent,
+    DashboardComponent,
+    AdminImagenCreateComponent
   ],
   imports: [
-    BrowserModule
+    routes,
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ImagenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
